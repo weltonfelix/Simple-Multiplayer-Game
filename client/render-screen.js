8da1c641dc55ff1d3a1e8b5c4b40312fe91a93eb
Welton Felix <contato.weltonf@gmail.com>
2020-12-31 18:09:49 -0300

    Modularize Javascript code

---
+++ client/render-screen.js
@@ -0,0 +1,23 @@
+export default function renderScreen(screen, state, requestAnimationFrame) {
+  const context = screen.getContext('2d');
+
+  context.clearRect(0, 0, 10, 10);
+
+  for (const playerId in state.players) {
+    const player = state.players[playerId];
+
+    context.fillStyle = 'black';
+    context.fillRect(player.x, player.y, 1, 1);
+  }
+
+  for (const fruitId in state.fruits) {
+    const fruit = state.fruits[fruitId];
+
+    context.fillStyle = 'green';
+    context.fillRect(fruit.x, fruit.y, 1, 1);
+  }
+
+  requestAnimationFrame(() => {
+    renderScreen(screen, state, requestAnimationFrame)
+  });
+}
